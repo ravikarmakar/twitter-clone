aca117d3ed189298f522b77dbbcacdbc81831c4b
Ravi Karmakar <ravikarmkar94475@gmail.com>
2024-10-06 21:33:57 +0530

    added authroutes and rest setup

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { signup, logout, login } from "../controllers/authControler.js";
+import { signup, logout, login, getMe } from "../controllers/authControler.js";
+import { ProtectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
+router.get("/me", ProtectRoute, getMe);
 router.post("/signup", signup);
-
 router.post("/login", login);
-
 router.post("/logout", logout);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 import authRoutes from "./routes/authRoutes.js";
@@ -8,6 +9,11 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
