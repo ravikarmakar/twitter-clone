5e3138400e67d04d7859fe8094d3daa3e7184eb5
Ravi Karmakar <ravikarmkar94475@gmail.com>
2024-10-30 16:48:07 +0530

    just testing for branch

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,8 @@ app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 app.use(cookieParser());
 
+console.log("ravi");
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
